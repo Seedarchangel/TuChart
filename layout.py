7c3e9cca153ee7b9cfee0729e7324187a9d5963b
RongZhou Li <rzli2@illinois.edu>
2017-08-28 00:22:12 -0500

    Addition
    Added project files

---
+++ layout.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'layout.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from qtpy import QtCore, QtWidgets,QtWidgets
+from qtpy.QtWebEngineWidgets import QWebEngineView
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtWidgets.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(943, 851)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        self.create_kline_page(MainWindow)
+        #self.stacked_layout = QtWidgets.QStackedLayout()
+        #self.stacked_layout.addWidget(self.create_kline_page(MainWindow))
+
+
+
+    def create_kline_page(self, MainWindow):
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setEnabled(True)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setContentsMargins(-1, -1, -1, 0)# I have no idea how layouts work
+        self.gridLayout.setHorizontalSpacing(0)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.dateEdit_2 = QtWidgets.QDateEdit(self.centralwidget) #End Date
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.dateEdit_2.sizePolicy().hasHeightForWidth())
+        self.dateEdit_2.setSizePolicy(sizePolicy)
+        self.dateEdit_2.setObjectName(_fromUtf8("dateEdit_2"))
+        self.gridLayout.addWidget(self.dateEdit_2, 4, 1, 1, 1)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 3, 0, 1, 1)
+        self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)#Start date
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.dateEdit.sizePolicy().hasHeightForWidth())
+        self.dateEdit.setSizePolicy(sizePolicy)
+        self.dateEdit.setObjectName(_fromUtf8("dateEdit"))
+        self.gridLayout.addWidget(self.dateEdit, 4, 0, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)  #Shows "enddate"
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 3, 1, 1, 1)
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.treeWidget = QtWidgets.QTreeWidget(self.centralwidget) #Displays stock according to business type
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(2)
+        sizePolicy.setHeightForWidth(self.treeWidget.sizePolicy().hasHeightForWidth())
+        self.treeWidget.setSizePolicy(sizePolicy)
+        self.treeWidget.setObjectName(_fromUtf8("treeWidget"))
+        self.treeWidget.headerItem().setText(0, _fromUtf8("历史数据"))
+        self.verticalLayout_3.addWidget(self.treeWidget)
+        self.gridLayout.addLayout(self.verticalLayout_3, 1, 0, 1, 3)
+        self.commandLinkButton = QtWidgets.QCommandLinkButton(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.commandLinkButton.sizePolicy().hasHeightForWidth())
+        self.commandLinkButton.setSizePolicy(sizePolicy)
+        self.commandLinkButton.setText(_fromUtf8(""))
+        self.commandLinkButton.setObjectName(_fromUtf8("commandLinkButton"))
+        self.gridLayout.addWidget(self.commandLinkButton, 4, 2, 1, 1)
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget) #Combobox for Selecting type of graph
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy)
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.gridLayout.addWidget(self.comboBox, 3, 2, 1, 1)
+        self.treeWidget_2 = QtWidgets.QTreeWidget(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.treeWidget_2.sizePolicy().hasHeightForWidth())
+        self.treeWidget_2.setSizePolicy(sizePolicy)#Shows what graphs are selected
+        self.treeWidget_2.setObjectName(_fromUtf8("treeWidget_2"))
+        self.treeWidget_2.headerItem().setText(0, _fromUtf8("绘图项"))
+        self.gridLayout.addWidget(self.treeWidget_2, 5, 0, 1, 3)
+        self.gridLayout.setColumnStretch(0, 60)
+        self.gridLayout.setColumnStretch(1, 20)
+        self.gridLayout.setColumnStretch(2, 20)
+        self.horizontalLayout.addLayout(self.gridLayout)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
+        self.widget.setSizePolicy(sizePolicy)
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.widget = QWebEngineView() #This is for displaying html content generated by pyecharts
+        self.verticalLayout.addWidget(self.widget)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.horizontalLayout.setStretch(0, 1)
+        self.horizontalLayout.setStretch(1, 15)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.menubar = QtWidgets.QToolBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 900, 23))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        #self.menubar.setNativeMenuBar(False)
+        self.combobox = QtWidgets.QComboBox()
+        self.menubar.addWidget(self.combobox)
+        self.combobox.addItems(["K线", "复权", "分笔数据"])
+        self.comboBox.setFixedSize(55,40)
+
+        MainWindow.addToolBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("Tuchart", "Tuchart", None))
+        self.label.setText(_translate("MainWindow", "Start", None))
+        self.label_2.setText(_translate("MainWindow", "End", None))
