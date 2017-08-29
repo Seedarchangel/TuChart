58b70de5dff82069c2bd9501608e6f41d73c9b4c
RongZhou Li <rzli2@illinois.edu>
2017-08-29 18:17:22 -0500

    commit setup

--- setup.py
+++ setup.py
@@ -1,13 +1,115 @@
-from distutils.core import setup
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import codecs
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'tuchart'
+DESCRIPTION = 'Graphing app for Chinese stocks. '
+URL = 'https://github.com/Seedarchangel/TuChart'
+EMAIL = 'rzli2@illinois.com'
+AUTHOR = 'Rong Zhou Li'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+"pyecharts","tushare"
+    # 'requests', 'maya', 'records',
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANFEST.in file!
+with codecs.open(os.path.join(here, 'README.MD'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec (f.read(), about)
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.sep.join(('.', 'dist')))
+        except FileNotFoundError:
+          pass
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-  name = 'tuchart',
-  packages = ['tuchart'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Visualization for Tushare using Echarts',
-  author = 'Rong Zhou Li',
-  author_email = 'rzli2@illinois.com',
-  url = 'hhttps://github.com/Seedarchangel/TuChart', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['echarts', 'tushare', 'pyqt'], # arbitrary keywords
-  classifiers = [],
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    data_files=[('Tuchart/class.json', ['Tuchart/time'])],
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'publish': PublishCommand,
+    },
 )
