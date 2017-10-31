3836a37c270f23f1aea82ee76d87b0a1f0a6beb8
cansijyun <can_sijyun@yahoo.co.jp>
2017-10-31 18:22:09 +0800

    add some functions
    add search,tree widget initiation and sort by code functions
    add indexes to series
    changes the pastQ to 30 days earlier
    changed the  "json" to pickle

--- Tuchart/main.py
+++ Tuchart/main.py
@@ -11,7 +11,10 @@ from pandas import DataFrame as df
 import pandas as pd
 import tushare as ts
 import cPickle
+import numpy as np
 import json
+import warnings
+warnings.filterwarnings("ignore")
 list1 = []
 
 
@@ -31,66 +34,56 @@ class MyUi(QMainWindow):
                     #cPickle.dump(datetime.now(),infile)
 
             else:
-                print("More than 12 hours. Updating Json...")
+                print("More than 12 hours. Updating pickle...")
                 data = ts.get_industry_classified()
-                #data.to_json(cwd + "/class.json", orient="columns")#writes class data so no need to call Tushare agian
-                with open("class.json","w+") as outfile:
+                with open("class","w+") as outfile:
                     cPickle.dump(data,outfile)
                 now = datetime.now()
                 with open("time", "w+") as outfile: #update time
                     cPickle.dump(now, outfile)
 
         else:
-            print("No json found!") #If this is first time using tuchart in this directory
+            print("No pickle found!") #If this is first time using tuchart in this directory
             data = df()
             data = ts.get_industry_classified()
-            #var = data.to_json(cwd+"/class.json",orient="columns")
-            with open('class.json', 'w+') as outfile: #records json
+            with open('class', 'w+') as outfile: #records pickle
                 cPickle.dump(data, outfile)
             now = datetime.now()
             with open("time", "w+") as outfile:
                 cPickle.dump(now,outfile)
 
-        with open("class.json", "r") as infile:  # reads current time
+        with open("class", "r") as infile:  # reads current time
             series = cPickle.load(infile)
         #series = pd.read_json(cwd + "\\class.json")
         #series = ts.get_industry_classified()
         series = pd.DataFrame(series)
         curdate = time.strftime("%Y/%m/%d") #gets current time to put into dateedit
         dateobj = datetime.strptime(curdate, "%Y/%m/%d")#converts to datetime object
-        past = dateobj - timedelta(days = 7)  #minus a week to start date
+        past = dateobj - timedelta(days = 30)  #minus a month to start date
         pasttime = datetime.strftime(past, "%Y/%m/%d")
         QPast = QDate.fromString(pasttime,"yyyy/MM/dd") #convert to qtime so that widget accepts the values
         Qcurdate = QDate.fromString(curdate,"yyyy/MM/dd")
-        list1 = series["c_name"].tolist()  #Get industry categories. Filters out redundant ones
-        list1 = list(set(list1))
+
+        np_indexes = np.array([['sh', '上证指数', '大盘指数'],
+                               ['sz', '深证成指', '大盘指数'],
+                               ['hs300', '沪深300指数', '大盘指数'],
+                               ['sz50', '上证50', '大盘指数'],
+                               ['zxb', '中小板', '大盘指数'],
+                               ['cyb', '创业板', '大盘指数']])
+        indexes = df(data=np_indexes,
+                     index=range(5000, 5006),
+                     columns=["code", "name", "c_name"])
+        series = indexes.append(series)
+        list1_bfr = series["c_name"].tolist()  #Get industry categories. Filters out redundant ones
+        list1 = list(set(list1_bfr))
+        list1.sort(key=list1_bfr.index)
         #w = database()
         #zsparent = QTreeWidgetItem(self.ui.treeWidget)
         #zsparent.setText(0,"股票指数")
         #zsnames =["上证指数-sh","深圳成指-sz","沪深300指数-hs300","上证50-"]
-        zsparent = QTreeWidgetItem(self.ui.treeWidget)
-        zsparent.setText(0, "股票指数")
-        zsnames = ["上证指数-sh", "深圳成指-sz", "沪深300指数-hs300", "上证50-sz50", "中小板-zxb", "创业板-cyb"]
-        for k in zsnames:
-            child = QTreeWidgetItem(zsparent)
-            child.setText(0, k)
 
-        for j in list1:
-            parent = QTreeWidgetItem(self.ui.treeWidget)  #populate treewidget with names
-            parent.setText(0,j)
-            var = series.loc[series["c_name"] == j]
-            list2 = var["code"].tolist()
-            name = var["name"].tolist()
+        self.init_treeWidget(list1,series)
 
-
-            #var = showcollection(i) #Display database items
-            for idx,val in enumerate(list2):
-                child = QTreeWidgetItem(parent)
-                child.setText(0, name[idx]+"-"+str(val))
-                #for i in Drag:
-                    #grandson = QTreeWidgetItem(child)     #Commented out because increases program response time
-                    #grandson.setText(0, i)
-        #self.ui.treeWidget.itemDoubleClicked.connect(self.onClickItem) #Display Collection items
         self.ui.treeWidget.setContextMenuPolicy(Qt.CustomContextMenu)
         self.ui.treeWidget.customContextMenuRequested.connect(self.openMenu)
 
@@ -98,7 +91,13 @@ class MyUi(QMainWindow):
         file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "render.html")) #path to read html file
         local_url = QUrl.fromLocalFile(file_path)
         self.ui.widget.load(local_url)
-        self.ui.commandLinkButton.setFixedSize(50, 50)
+        #self.ui.commandLinkButton.setFixedSize(50, 50)
+
+
+        self.ui.search_btn.clicked.connect(lambda: self.search_comp(series))
+        self.ui.init_code_btn.clicked.connect(lambda: self.code_sort_tree(series))
+        self.ui.init_category_btn.clicked.connect(lambda: self.init_treeWidget(list1, series))
+
         self.ui.commandLinkButton.clicked.connect(self.classify)  #when the arrow button is clicked, trigger events
 
 
@@ -116,9 +115,6 @@ class MyUi(QMainWindow):
             self.ui.label_2.setSizePolicy(retain_size)
         except AttributeError:
             print("No PYQT5 Binding! Widgets might be deformed")
-
-
-
         self.ui.dateEdit.setDate(QPast)
         self.ui.dateEdit_2.setDate(Qcurdate)#populate widgets
         self.ui.dateEdit.setCalendarPopup(True)
@@ -129,6 +125,54 @@ class MyUi(QMainWindow):
         self.ui.treeWidget_2.customContextMenuRequested.connect(self.openWidgetMenu)
         #self.ui.toolbutton.clicked.connect(lambda action: self.graphmerge(action, CombineKeyword))
         self.ui.combobox.currentIndexChanged.connect(self.modifycombo)
+
+    def init_treeWidget(self, list1, series):
+
+        self.ui.treeWidget.clear()
+        self.ui.treeWidget.headerItem().setText(0, "历史综合数据")
+
+        for j in list1:
+            parent = QTreeWidgetItem(self.ui.treeWidget)  #populate treewidget with names
+            parent.setText(0,j)
+            var = series.loc[series["c_name"] == j]
+            list2 = var["code"].tolist()
+            name = var["name"].tolist()
+            #var = showcollection(i) #Display database items
+            for idx,val in enumerate(list2):
+                child = QTreeWidgetItem(parent)
+                child.setText(0, name[idx]+"-"+str(val))
+                #for i in Drag:
+                    #grandson = QTreeWidgetItem(child)     #Commented out because increases program response time
+                    #grandson.setText(0, i)
+        #self.ui.treeWidget.itemDoubleClicked.connect(self.onClickItem) #Display Collection items
+
+    def code_sort_tree(self, companies):
+        self.ui.treeWidget.headerItem().setText(0, "历史综合数据")
+        self.ui.treeWidget.clear()
+        sorted_comps = companies.sort_values(["code"])
+        code_list = sorted_comps["code"].tolist()
+        name_list = sorted_comps["name"].tolist()
+        shares_parent = QTreeWidgetItem(self.ui.treeWidget)
+        shares_parent.setText(0, "个股行情")
+        for idx, val in enumerate(code_list):
+            child = QTreeWidgetItem(shares_parent)
+            child.setText(0, name_list[idx] + "-" + str(val))
+        self.ui.treeWidget.expandToDepth(0)
+
+    def search_comp(self, companies):
+        self.ui.treeWidget.clear()
+        text = self.ui.search_lineEdit.text()
+        filtered_codes = companies[companies['code'].str.contains(text)]
+        filtered_names = companies[companies['name'].str.contains(text)]
+        filtered_comps = filtered_codes.append(filtered_names)
+        code_list = filtered_comps["code"].tolist()
+        name_list = filtered_comps["name"].tolist()
+        parent = QTreeWidgetItem(self.ui.treeWidget)
+        parent.setText(0, "搜索结果")
+        for idx, val in enumerate(code_list):
+            child = QTreeWidgetItem(parent)
+            child.setText(0, name_list[idx] + "-" + str(val))
+        self.ui.treeWidget.expandToDepth(0)
 
 
     def modifycombo(self):
@@ -283,8 +327,6 @@ class MyUi(QMainWindow):
     def openMenu(self,position):
         indexes = self.ui.treeWidget.selectedIndexes()
         item = self.ui.treeWidget.itemAt(position)
-
-
         db_origin = ""
         #if item.parent():
          #   db_origin = item.parent().text(0)
